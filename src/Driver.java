af3fc02d409ac5297ea0fa8bafaa8aa8a6ac30de
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-02-04 04:24:45 +0530

    new driver created for linkedList2

---
+++ src/Driver.java
@@ -0,0 +1,38 @@
+public class Driver {
+    public static void main(String[] args) {
+        LinkedList2 list=new LinkedList2();
+        System.out.println("a)");
+        list.insertNodeFront(20);
+        list.insertNodeFront(30);
+        list.insertNodeFront(40);
+        list.insertNodeFront(50);
+        list.displayNodes();
+
+        System.out.println("b)");
+        list.insertNodeFront(10);
+        list.displayNodes();
+
+        System.out.println("c)");
+        list.insertNodeFront(60);
+        list.displayNodes();
+
+        System.out.println("d)");
+        list.insertNext(30,70);
+        list.displayNodes();
+
+        System.out.println("e)");
+        list.displayNodes();
+
+        System.out.println("f)");
+        list.deleteNode(70);
+        list.displayNodes();
+
+        System.out.println("g)");
+        if(list.search(30)==null){
+            System.out.println("value not found");
+        }else{
+            System.out.println("value found");
+        }
+
+    }
+}
